5015fbe903e4f2a028ab2b577826a0fa4b154481
MDandersonm <1215kimyj@gmail.com>
2023-05-15 14:42:50 +0900

    init: [김영진] 저장소 초기화

---
+++ react-1hour-pumgyeok/src/pages/Home.js
@@ -0,0 +1,22 @@
+//리액트 컴퍼넌트엔 함수형컴퍼넌트 클래스형컴퍼넌트 2가지가있다
+
+// //함수형 컴퍼넌트
+import React from "react";
+function Home(){
+    return <h1>Home화면입니다.</h1>
+}
+
+export default Home;
+
+
+//클래스형컴퍼넌트
+
+// import React,{Component} from "react";
+
+// class Home extends Component {
+//     render(){
+//         return <h1>Home 화면 입니다.</h1>
+//     }
+// }
+
+// export default Home;
