ae8b75c2cb5fcd9488ac461a33f6449a9e9e9b14
MDandersonm <1215kimyj@gmail.com>
2023-05-17 19:46:44 +0900

    feat: [김영진] 리액트-코지코더 기초;  useEffect 사용법 [REACT-KOSSIE-BASIC-XX]

---
+++ kossie_coder_react_basic/src/pages/UseEffect.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+const UseEffect = () => {
+  const [count, setCount] = useState(0);
+  const [kossie, setKossie] = useState(0);
+  useEffect(() => {
+    console.log("effect함수실행1");
+  }); //렌더링되고 실행시켜줌
+  
+  useEffect(() => {
+    console.log("effect함수실행2");
+  },[count]); //count가 변경되었을때만 실행
+
+  useEffect(() => {
+    console.log("effect함수실행3");
+  },[count,kossie]); //count,kossie가 변경되었을때만 실행
+  useEffect(() => {
+    console.log("first useEffect");
+  },[]); //첫렌더링에만 실행
+
+
+
+  console.log("lendering",count);
+  // state가 변경될때마다 컴퍼넌트가 리 랜더링
+
+  const increment =() =>{
+    setCount(count+1)
+  }
+  //click 누를때마다  state가 변경될때마다 컴퍼넌트가 리랜더링일어남 그후 useEffect실행시켜줌
+  return (
+    <div>
+      <h1>Kossie Coder</h1>
+      <button onClick={increment}>Click</button>
+      <button onClick={()=>setKossie(kossie+1)}>Click1</button>
+      {/* state이기때문에 누를때마다 리렌더링이 일어나고 useEffect가 실행됨. */}
+    </div>
+  );
+};
+export default UseEffect;
