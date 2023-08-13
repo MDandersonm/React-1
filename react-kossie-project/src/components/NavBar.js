c20c45fa906b569742441233e62c8085432dfb96
MDandersonm <1215kimyj@gmail.com>
2023-08-14 02:40:03 +0900

    feat: [김영진] NavBar 내의  Link를 NavLink로 변경 [REACT-KOSSIEBLOG-03]

--- react-kossie-project/src/components/NavBar.js
+++ react-kossie-project/src/components/NavBar.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 const NavBar = () => {
-    return (
-        <nav className="navbar navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            Home
-          </Link>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/blogs">
-                Blogs
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink
+              //exact
+              activeClassName="active"
+              className="nav-link"
+              aria-current="page"
+              to="/blogs"
+            >
+              Blogs
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
