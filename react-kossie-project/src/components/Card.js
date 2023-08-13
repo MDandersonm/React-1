dded425a90bad902b8beb71cc8df995006eb79b1
MDandersonm <1215kimyj@gmail.com>
2023-08-14 05:41:38 +0900

    feat: [김영진] 블로그 List페이지에서 글을 선택하면 수정페이지로 이동  [REACT-KOSSIEBLOG-06]

--- react-kossie-project/src/components/Card.js
+++ react-kossie-project/src/components/Card.js
@@ -1,15 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 // const Card = (props) => {//props객체 사용방법
-const Card = ({ title, children }) => {
+const Card = ({ title, children, onClick }) => {
   console.log({ children });
   //구조분해 방법
 
   return (
-    <div className="card my-3">
+    <div className="card my-3 cursor-pointer" onClick={onClick}>
       {/* <div className="card-body">{props.title}</div> */}
       <div className="card-body">
-        {title}
+        {/* {title} */}
         {children && <div>{children}</div>}
         {/* //children이 존재하면 출력하라는말 */}
       </div>
@@ -19,12 +19,14 @@ const Card = ({ title, children }) => {
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,//필수값으로 설정
-  children:PropTypes.element//<div>같은데에 감싸야한다는 말.
+  children:PropTypes.element,//<div>같은데에 감싸야한다는 말.
+  onClick:PropTypes.func,
 };
 
 Card.defaultProps={//기본값 설정 Card컴퍼넌트에 title={post.title}이 없을경우
     title:'타이틀',
-    children:null
+    children:null,
+    onClick: ()=>{}
 }
 
 export default Card;
--- react-kossie-project/src/pages/ListPage.js
+++ react-kossie-project/src/pages/ListPage.js
@@ -2,10 +2,10 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 const ListPage = () => {
   const [posts, setPosts] = useState([]);
-
+  const history = useHistory();
   const getPosts = () => {
     axios.get("http://localhost:3001/posts").then((res) => {
       console.log(res);
@@ -22,17 +22,28 @@ const ListPage = () => {
       <div className="my-5 d-flex justify-content-between">
         <h1>Blogs</h1>
         <div>
-          <Link to="/blogs/create" className="btn btn-success">Create New</Link>
+          <Link to="/blogs/create" className="btn btn-success">
+            Create New
+          </Link>
         </div>
       </div>
 
       {posts.map((post) => {
         return (
-          <Card title={post.title} key={post.id}>
+          <Card
+            title={post.title}
+            key={post.id}
+            onClick={() => history.push("/blogs/edit")}
+          >
+            {/* 함수도 props로 내려줄수있다. */}
             {/* props의 children 으로 자식컴퍼넌트에서 받을수있다. */}
             <div className="d-flex justify-content-between">
               <div>{post.title}</div>
-              <button>button</button>
+              <div>
+                <button className="btn btn-danger btn-sm" onClick={(e)=>{
+                    e.stopPropagation();//부모컴퍼넌트의 이벤트발생을 막아줌
+                    console.log("delete")}}>Delete</button>
+              </div>
             </div>
           </Card>
           //   <div className="card my-3" key={post.id}>
