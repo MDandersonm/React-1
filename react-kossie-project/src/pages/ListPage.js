82aed07403352f6833c226101a8764fc51bf3972
MDandersonm <1215kimyj@gmail.com>
2023-08-14 05:04:32 +0900

    feat: [김영진] 블로그 List페이지에서 create버튼을 만들어서 글쓰기버튼 만들기 [REACT-KOSSIEBLOG-05]

--- react-kossie-project/src/pages/ListPage.js
+++ react-kossie-project/src/pages/ListPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
+import { Link } from "react-router-dom";
 const ListPage = () => {
   const [posts, setPosts] = useState([]);
 
@@ -18,15 +19,20 @@ const ListPage = () => {
 
   return (
     <div>
-      <h1 className="my-5">Blogs</h1>
+      <div className="my-5 d-flex justify-content-between">
+        <h1>Blogs</h1>
+        <div>
+          <Link to="/blogs/create" className="btn btn-success">Create New</Link>
+        </div>
+      </div>
+
       {posts.map((post) => {
         return (
           <Card title={post.title} key={post.id}>
-
             {/* props의 children 으로 자식컴퍼넌트에서 받을수있다. */}
             <div className="d-flex justify-content-between">
-                <div>{post.title}</div>
-                <button>button</button>
+              <div>{post.title}</div>
+              <button>button</button>
             </div>
           </Card>
           //   <div className="card my-3" key={post.id}>
