082648705a2355ce6cc096ad148579f4357c4475
MDandersonm <1215kimyj@gmail.com>
2023-08-14 02:39:34 +0900

    feat: [김영진] route설정 추가 [REACT-KOSSIEBLOG-02]

--- react-kossie-project/src/App.js
+++ react-kossie-project/src/App.js
@@ -1,33 +1,40 @@
 // import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import HomePage from "./pages/HomePage";
-import CreatePage from "./pages/CreatePage";
-import EditPage from "./pages/EditPage";
-import ListPage from "./pages/ListPage";
-
+import routes from "./routes";
 
 function App() {
   return (
     // <React.Fragment>
     <BrowserRouter>
       <NavBar></NavBar>
+
       <div className="container my-3">
         <Switch>
           {/*switch :  route가 매치되는 순간 뒷 route는 확인하지않음 */}
           {/* exact: / 라고 정확하게 일치해야함 안그러면 /blog도  /로 매칭이됨 */}
-          <Route path="/" exact>
-            <HomePage/>
-          </Route>
-          <Route path="/blogs" exact>
-            <ListPage></ListPage>
-          </Route>
-          <Route path="/blogs/create" exact>
-            <CreatePage></CreatePage>
-          </Route>
-          <Route path="/blogs/edit" exact>
-            <EditPage></EditPage>
-          </Route>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}//url이 각각 다르기 때문
+                exact
+                path={route.path}
+                component={route.component}
+              ></Route>
+              /* <Route path="/" exact>
+                <HomePage />
+                </Route>,
+                <Route path="/blogs" exact>
+                <ListPage></ListPage>
+                </Route>,
+                <Route path="/blogs/create" exact>
+                <CreatePage></CreatePage>
+                </Route>,
+                <Route path="/blogs/edit" exact>
+                <EditPage></EditPage>
+                </Route>, */
+            );
+          })}
         </Switch>
       </div>
     </BrowserRouter>
@@ -35,5 +42,4 @@ function App() {
     // </React.Fragment>
   );
 }
-
 export default App;
