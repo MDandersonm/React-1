8555225a1b0908fd2548ee70041b0527fe37096a
MDandersonm <1215kimyj@gmail.com>
2023-08-14 04:57:11 +0900

    add: [김영진] 블로그 Card 컴퍼넌트에 Prop types를 설정해주기  [REACT-KOSSIEBLOG-04]

--- react-kossie-project/src/components/Card.js
+++ react-kossie-project/src/components/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 // const Card = (props) => {//props객체 사용방법
 const Card = ({ title, children }) => {
   console.log({ children });
@@ -16,5 +16,15 @@ const Card = ({ title, children }) => {
     </div>
   );
 };
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,//필수값으로 설정
+  children:PropTypes.element//<div>같은데에 감싸야한다는 말.
+};
+
+Card.defaultProps={//기본값 설정 Card컴퍼넌트에 title={post.title}이 없을경우
+    title:'타이틀',
+    children:null
+}
 
 export default Card;
