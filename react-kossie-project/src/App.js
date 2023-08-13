ca85d111d6ef1cfc0b665901b1eaf605f5537711
MDandersonm <1215kimyj@gmail.com>
2023-08-13 23:56:53 +0900

    feat: [김영진] 누구에게나 보이는 NavBar 헤더 [REACT-KOSSIEBLOG-01]

--- react-kossie-project/src/App.js
+++ react-kossie-project/src/App.js
@@ -1,35 +1,26 @@
 // import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import BlogForm from './components/BlogForm'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import BlogForm from "./components/BlogForm";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     // <React.Fragment>
     <BrowserRouter>
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            Home
-          </Link>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/blogs">
-                Blogs
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <NavBar></NavBar>
       <div className="container my-3">
-      <Switch>
-        {/*switch :  route가 매치되는 순간 뒷 route는 확인하지않음 */}
-        {/* exact: / 라고 정확하게 일치해야함 안그러면 /blog도  /로 매칭이됨 */}
-        <Route path="/" exact>
-          Home Page
-        </Route>
-        <Route path="/blogs">
-          <BlogForm/>
-        </Route>
-      </Switch>
+        <Switch>
+          {/*switch :  route가 매치되는 순간 뒷 route는 확인하지않음 */}
+          {/* exact: / 라고 정확하게 일치해야함 안그러면 /blog도  /로 매칭이됨 */}
+          <Route path="/" exact>
+            Home Page
+          </Route>
+          <Route path="/blogs">
+            BlogList
+          </Route>
+          <Route path="/blogs/create">
+            <BlogForm />
+          </Route>
+        </Switch>
       </div>
     </BrowserRouter>
 
