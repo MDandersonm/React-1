6267a7e81ae4fdc06a834b439b9279f3698e00f6
MDandersonm <1215kimyj@gmail.com>
2023-08-16 12:18:12 +0900

    feat: [김영진] 잘못된 url로 접속시 404 페이지 나오게[REACT-KOSSIEBLOG-22]

--- react-kossie-project/src/routes.js
+++ react-kossie-project/src/routes.js
@@ -4,6 +4,7 @@ import EditPage from "./pages/EditPage";
 import ListPage from "./pages/ListPage";
 import ShowPage from "./pages/ShowPage";
 import AdminPage from "./pages/AdminPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 
 const routes = [
@@ -35,6 +36,10 @@ const routes = [
       path: "/blogs/:id",//순서가 중요하다 create가 위에있어야 create일때 id를 안보여주고 create페이지를 보여줌
       component: ShowPage,
     },
+    {
+      path:'*',
+      component:NotFoundPage
+    }
     
   ];
 
